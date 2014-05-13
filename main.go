ef1779c19507b5a69a4e70abe91601efa9738986
Anup Chenthamarakshan <anupcshan@gmail.com>
2014-05-13 15:37:00 -0700

    Restructuring packages to make it testable.

--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"github.com/anupcshan/sciforme/task"
 	"github.com/codegangsta/cli"
+	"github.com/golang/glog"
 	"github.com/jmcvetta/neoism"
-  "github.com/golang/glog"
 	"os"
 )
 
@@ -14,32 +15,24 @@ func main() {
 		glog.Fatalf("DB Error: %q", err)
 	}
 
+	tm := task.TaskManager{Database: db}
+
 	app := cli.NewApp()
-	app.Name = "todo"
+	app.Name = "task"
 	app.Usage = "task list on the command line"
-  app.Version = "0.1"
+	app.Version = "0.1"
 	app.Commands = []cli.Command{
 		{
 			Name:      "add",
 			ShortName: "a",
 			Usage:     "Add a task to the list",
 			Action: func(c *cli.Context) {
-        if !c.Args().Present() {
-          glog.Error("Add: Please provide description of task to add")
-        }
+				if !c.Args().Present() {
+					glog.Fatal("Add: Please provide description of task to add")
+				}
 
-        tName := c.Args().First()
-
-        td, err := db.CreateNode(neoism.Props{"name": tName})
-        td.AddLabel("Task")
-
-        if err != nil {
-          glog.Fatalf("DB Error: %q", err)
-        }
-
-				if glog.V(2) {
-          glog.Info("Added task: ", tName)
-        }
+				tName := c.Args().First()
+				tm.AddTask(tName)
 			},
 		},
 		{
@@ -47,7 +40,7 @@ func main() {
 			ShortName: "ls",
 			Usage:     "List of tasks to be completed",
 			Action: func(c *cli.Context) {
-        glog.Fatal("TODO: list")
+				glog.Fatal("TODO: list")
 			},
 		},
 	}
---
+++ task/task.go
@@ -0,0 +1,29 @@
+package task
+
+import (
+	"fmt"
+	"github.com/golang/glog"
+	"github.com/jmcvetta/neoism"
+)
+
+type Task struct {
+}
+
+type TaskManager struct {
+	Database *neoism.Database
+}
+
+func (self TaskManager) AddTask(desc string) error {
+	td, err := self.Database.CreateNode(neoism.Props{"name": desc})
+	td.AddLabel("Task")
+
+	if err != nil {
+		return fmt.Errorf("DB Error: %q", err)
+	}
+
+	if glog.V(2) {
+		return fmt.Errorf("Added task: ", desc)
+	}
+
+	return nil
+}
